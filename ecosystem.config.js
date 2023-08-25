bcc44dcb85ea31e1ebba7a404b79b11f74e41e23
asliddin750750 <asliddin750750@gmail.com>
2023-08-25 16:10:14 +0500

    ...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'Problems',
-            port: process.env.PORT || 3000,
+            port: process.env.PORT || 3004,
             exec_mode: 'cluster',
             instances: 'max',
             script: './.output/server/index.mjs'
